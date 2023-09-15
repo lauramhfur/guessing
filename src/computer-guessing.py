7ac5dd507e7be7b08e7ee7e277fc01a70be576b9
Laura <lauramhfur@gmail.com>
2023-09-15 10:10:29 +0200

    CTiB Exercises Week 3

--- src/computer-guessing.py
+++ src/computer-guessing.py
@@ -23,6 +23,8 @@ print("Let me know how good my guess is.\n")
 # Here, we implement the computer's strategy for guessing
 # the number you are thinking of. Don't lie to the
 # computer. It won't punish you, but it will frown upon it.
+
+# First strategy - going from 1 to 20
 for guess in range(1, 21):
     result = input_selection(
         "I'm guessing {}\nHow is my guess?".format(guess),
@@ -33,3 +35,32 @@ for guess in range(1, 21):
         break
 
     print("I must have been too low, right?", result)
+
+# Second strategy - starting at 20
+for i in range(20, 0, -1): # doesn't include 0
+    result = input_selection(
+        "I'm guessing {}\nHow is my guess?".format(guess),
+        ["low", "hit", "high"]
+    )
+    if result == "hit":
+        print("Wuhuu!")
+        break
+
+    print("I must have been too low, right?", result)
+
+# Third strategy - starting by guessing 10 and using the middle of the interval
+upper_bound = 20
+lower_bound = 1
+for guess in range(lower_bound, upper_bound + 1):
+    guess = (upper_bound + lower_bound) // 2
+    result = input_selection(
+        "I'm guessing {}\nHow is my guess?".format(guess),
+        ["low", "hit", "high"]
+    )
+    if result == "low":
+        lower_bound = guess + 1
+    if result == "high":
+        upper_bound = guess - 1
+    if result == "hit":
+        print("Wuhuu!")
+        break
